65b44619f19f4a3994c3be0a03a5a3379236f6cc
Camden Sladcik <sladcikc@oregonstate.edu>
2018-05-31 21:32:01 -0700

    make picks, words on index

--- nbts/index.php
+++ nbts/index.php
@@ -16,6 +16,7 @@
     </head>
     <body>
         <section>
+            <p> Can you beat the MLB hitting streak of 56 games? Correctly pick 57 different hitters to get hits!</p>
                 <form>
                     <input type="button" value="Sign Up" onclick="window.location.href='./signUp.php'" />
                     <input type="button" value="Log In" onclick="window.location.href='./login.php'" />
